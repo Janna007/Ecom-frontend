1fe51e208ce6d462648b1d8e236a804086f1ac37
Janna007 <jannakondeth5@gmail.com>
2025-11-19 20:59:51 +0530

    design login page

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { router } from './router'
 import { RouterProvider } from 'react-router-dom'
+import 'antd/dist/reset.css'
+import { ConfigProvider } from 'antd'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ConfigProvider
+      theme={
+        {
+          token:{
+            colorPrimary:'#B55638',
+            colorLink:'#B55638'
+          }
+        }
+      }
+    >
+     
     <RouterProvider router={router} />
+    </ConfigProvider>
   </StrictMode>,
 )
